c8d286aaed0b61cfc24c1bc76244fb2aa23806fa
jan <jan.reyes.contact@gmail.com>
2025-03-04 14:12:18 -0600

    feat: added endpoint to overwrite the route

--- routers/routes.py
+++ routers/routes.py
@@ -20,6 +20,12 @@ router = APIRouter(
 
 
 @router.get("/get", response_model=Route)
-async def read_routes(request: Request):
+async def read_route(request: Request):
     route: Route = request.state.data["route"]
+    return JSONResponse(status_code=200, content=route.model_dump())
+
+
+@router.post("/edit", response_model=Route)
+async def edit_route(request: Request, route: Route):
+    request.state.data["route"] = route
     return JSONResponse(status_code=200, content=route.model_dump())
