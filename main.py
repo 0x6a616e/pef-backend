cb9f0c42ebc7def5202500ea5a64581e8b20404e
jan <jan.reyes.contact@gmail.com>
2025-03-03 19:47:20 -0600

    feat: added state and get endpoint
    added global state to have the current route and added an endpoint to
    get the current route

---
+++ main.py
@@ -0,0 +1,19 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+from .routers import routes
+
+
+@asynccontextmanager
+async def lifespan(_ : FastAPI):
+    state = {
+        "route" : routes.Route()
+    }
+    yield { "data" : state }
+
+
+app = FastAPI(
+    title="PEF",
+    lifespan=lifespan
+)
+app.include_router(routes.router)
---
+++ routers/routes.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Request
+
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from pydantic_extra_types.coordinate import Coordinate
+
+
+class Point(BaseModel):
+    coordinate : Coordinate
+
+class Route(BaseModel):
+    points : list[Point] = []
+
+router = APIRouter(
+    prefix="/routes",
+    tags=["routes"],
+)
+
+@router.get("/get", response_model=Route, responses={})
+async def read_routes(request : Request):
+    route : Route = request.state.data["route"]
+    return JSONResponse(status_code=200, content=route.model_dump())
