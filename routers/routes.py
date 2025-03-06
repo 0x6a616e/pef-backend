d4c34b728fc172656a311bf71ff58e0c2de4cc35
jan <jan.reyes.contact@gmail.com>
2025-03-05 20:45:02 -0600

    feat: added another search strategy

--- routers/routes.py
+++ routers/routes.py
@@ -75,6 +75,11 @@ def optimize_route(mission: Mission) -> Mission:
     search_parameters.first_solution_strategy = (
         routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
     )
+    search_parameters.local_search_metaheuristic = (
+        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
+    )
+    search_parameters.time_limit.seconds = 5  # TODO: Decidir tiempo
+    # search_parameters.log_search = True
 
     solution = routing.SolveWithParameters(search_parameters)
     if solution:
