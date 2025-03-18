718005addc0bab73afafe8cbcf61a00861922a25
jan <jan.reyes.contact@gmail.com>
2025-03-17 22:23:18 -0600

    feat: added cors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import routes
+
+origins = [
+    "https://pef.estupideznatural.tech",
+    "*",    # Development
+]
 
 
 app = FastAPI(
     title="PEF",
 )
 app.include_router(routes.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
