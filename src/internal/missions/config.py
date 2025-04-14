f0daddf1f862c066b4513885cc1363dd3a180f18
jan <jan.reyes.contact@gmail.com>
2025-04-14 17:48:51 -0600

    feat: changed default values for config instance

--- src/internal/missions/config.py
+++ src/internal/missions/config.py
@@ -2,9 +2,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    model_path: str = str()
-    images_folder: str = str()
-    image_size: tuple[int, int] = tuple()
+    model_path: str = ""
+    images_folder: str = ""
+    mongo_url: str = ""
+    image_size: tuple[int, int] = (680, 382)
 
 
 settings = Settings()
